29583570ef2e0f6fd8853490ae79e9f55d7099ab
zhangyoubo <13818956024@163.com>
2017-06-29 19:35:11 +0800

    2017.6.29

--- Blocks/ViewController.m
+++ Blocks/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "BlocksKit.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSArray *array = [NSArray arrayWithObjects:@"zhang",@"you",@"bo", nil];
+    
+//    [array bk_each:^(id obj){
+//        NSLog(@"obj_bk_each = %@",obj);
+//    }];
+//    
+//    [array bk_apply:^(id obj){
+//        NSLog(@"obj_bk_apply = %@",obj);
+//    }];
+    
+//    NSString *str = [array bk_match:^ BOOL (id obj){
+//        if ([obj isEqualToString:@"bo"]) {
+//            return YES;
+//        }
+//        return NO;
+//    }];
+//    
+//    NSLog(@"str = %@",str);
+    
+    
+//    NSArray * arr = [array bk_select:^ BOOL (id obj){
+//        if ([obj isEqualToString:@"bo"]||[obj isEqualToString:@"zhang"]) {
+//            return YES;
+//        }
+//        return NO;
+//    }];
+//    
+//    NSLog(@"arr = %@",arr);
+    
+    
+//    NSArray * arr = [array bk_map:^ id (id obj){
+//        return obj;
+//    }];
+//    
+//    NSLog(@"arr = %@",arr);
+    
+//    NSArray * arr = [array bk_compact:^ id (id obj){
+//        return obj;
+//    }];
+//    
+//    NSLog(@"arr = %@",arr);
+    [NSTimer bk_scheduleTimerWithTimeInterval:2 repeats:YES usingBlock:^(NSTimer *timer){
+        
+        NSLog(@"fireDate = %@",timer.fireDate);
+        
+    }];
+    
+//    [NSTimer scheduledTimerWithTimeInterval:3.0 repeats:YES block:^(NSTimer *timer){
+//        
+//        NSLog(@"fireDate = %@",timer.fireDate);
+//        
+//    }];
+    
 }
 
 
